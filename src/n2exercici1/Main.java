43d01b5bf6cbac104530d2aa80a6fe64295ac42f
ArnauBurguera <aburguera.ab@gmail.com>
2023-03-27 10:39:27 +0200

    n2exercici1 acabat

---
+++ src/n2exercici1/Main.java
@@ -0,0 +1,14 @@
+package n2exercici1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Smartphone smartphone1 = new Smartphone("Samsung","Galaxy");
+		
+		smartphone1.alarma();
+		smartphone1.fotografiar();
+		
+	}
+
+}
---
+++ src/n2exercici1/Telefon.java
@@ -0,0 +1,32 @@
+package n2exercici1;
+
+public class Telefon {
+	
+	private String marca;
+	private String model;
+	
+	public Telefon(String marca, String model) {
+		this.marca = marca;
+		this.model = model;
+	}
+	
+	public String getMarca() {
+		return this.marca;
+	}
+	public String getModel() {
+		return this.model;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public void trucar(String numTel) {
+		System.out.println("S'està trucant al numero " + numTel + ".");
+	}
+	
+	
+
+}
